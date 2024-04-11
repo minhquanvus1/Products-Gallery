02f204cfcf2c697960c84b171b8f5d434246ad26
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-04-11 14:56:50 +0700

    add the ProductsGalleryPage component to the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,22 @@
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 // import './App.css'
-
+import ProductsGalleryPage from "./components/ProductsGalleryPage";
 import React from "react";
-
+import TestProductsList from "./components/TestProductsList";
+import LearnInfScroll from "./components/LearnInfScroll";
+import LearnReactScroll from "./components/LearnReactScroll";
+import LearnPage from "./components/ProductsGalleryPage";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      {/* <LearnPage></LearnPage> */}
+      {/* <LearnReactScroll></LearnReactScroll> */}
+      {/* <LearnInfScroll></LearnInfScroll> */}
+      {/* <TestProductsList></TestProductsList> */}
+      <ProductsGalleryPage></ProductsGalleryPage>
+    </div>
+  );
 };
 
 export default App;
