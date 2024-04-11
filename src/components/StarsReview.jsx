657455c546e49f4a35626f261955e8c85bcec8ae
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-04-11 23:16:25 +0700

    implement the StarsReview logic to dynamically display the number of fullStars, halfStars, and emptyStars basing on the roundedRating value

--- src/components/StarsReview.jsx
+++ src/components/StarsReview.jsx
@@ -1,9 +1,33 @@
 import React from "react";
 
 const StarsReview = ({ roundedRating, size }) => {
+  let fullStars = 0;
+  let halfStars = 0;
+  let emptyStars = 0;
+
+  if (roundedRating !== undefined && roundedRating >= 0 && roundedRating <= 5) {
+    for (let i = 0; i < 5; i++) {
+      if (roundedRating >= 1) {
+        fullStars++;
+        roundedRating--;
+      } else if (roundedRating === 0.5) {
+        halfStars++;
+        roundedRating -= 0.5;
+      } else if (roundedRating === 0) {
+        emptyStars++;
+      } else {
+        break;
+      }
+    }
+  } else {
+    console.error("Invalid rating value");
+    console.log("roundedRating: ", typeof roundedRating);
+    console.log("roundedRating: ", roundedRating);
+    emptyStars = 5;
+  }
   return (
     <div>
-      {Array.from({ length: 3 }).map((_, i) => (
+      {Array.from({ length: fullStars }).map((_, i) => (
         <svg
           key={i}
           xmlns="http://www.w3.org/2000/svg"
@@ -18,7 +42,7 @@ const StarsReview = ({ roundedRating, size }) => {
         </svg>
       ))}
 
-      {Array.from({ length: 1 }).map((_, i) => (
+      {Array.from({ length: halfStars }).map((_, i) => (
         <svg
           key={i}
           xmlns="http://www.w3.org/2000/svg"
@@ -32,7 +56,7 @@ const StarsReview = ({ roundedRating, size }) => {
           <path d="M5.354 5.119 7.538.792A.516.516 0 0 1 8 .5c.183 0 .366.097.465.292l2.184 4.327 4.898.696A.537.537 0 0 1 16 6.32a.548.548 0 0 1-.17.445l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256a.52.52 0 0 1-.146.05c-.342.06-.668-.254-.6-.642l.83-4.73L.173 6.765a.55.55 0 0 1-.172-.403.58.58 0 0 1 .085-.302.513.513 0 0 1 .37-.245l4.898-.696zM8 12.027a.5.5 0 0 1 .232.056l3.686 1.894-.694-3.957a.565.565 0 0 1 .162-.505l2.907-2.77-4.052-.576a.525.525 0 0 1-.393-.288L8.001 2.223 8 2.226v9.8z" />
         </svg>
       ))}
-      {Array.from({ length: 1 }).map((_, i) => (
+      {Array.from({ length: emptyStars }).map((_, i) => (
         <svg
           key={i}
           xmlns="http://www.w3.org/2000/svg"
