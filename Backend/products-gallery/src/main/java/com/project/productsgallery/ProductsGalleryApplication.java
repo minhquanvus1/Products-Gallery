ba94ff22d7345252174bff29a73b6e5aaf7ec834
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-04-13 14:58:51 +0700

    create the Spring Boot Backend folder

---
+++ Backend/products-gallery/src/main/java/com/project/productsgallery/ProductsGalleryApplication.java
@@ -0,0 +1,13 @@
+package com.project.productsgallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsGalleryApplication.class, args);
+	}
+
+}
---
+++ Backend/products-gallery/src/test/java/com/project/productsgallery/ProductsGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.productsgallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
