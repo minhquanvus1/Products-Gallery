3f5099bf111a0da45d02ff97156d7c9f9e8026fc
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-04-13 10:29:29 +0700

    create the ProductModel so that it can be used as Type Annotation

---
+++ src/models/ProductModel.ts
@@ -0,0 +1,3 @@
+export class ProductModel {
+    constructor(public id: number, public title: string, public images: string[], public category: string, public description: string, public rating: number, public price: number) {}
+}
