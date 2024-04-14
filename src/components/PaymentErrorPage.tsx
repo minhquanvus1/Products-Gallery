381f072b75431be9c9378b847d628481daae8b37
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-04-14 10:01:00 +0700

    create the PaymentSuccessPage and PagementErrorPage as the page for PayPal to redirect payer to, depending on they have made the payment successful or not

---
+++ src/components/PaymentErrorPage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const PaymentErrorPage = () => {
+  return (
+    <div className="container">
+      <h1 className="text-center mt-5">Paypal Success</h1>
+      <div className="d-flex justify-content-center mt-5">
+        <Link to="/" className="btn btn-primary">
+          Back to Home
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default PaymentErrorPage;
