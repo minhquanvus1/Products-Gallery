b9fe3eea9be908e1d057fab50a720c460392f60c
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-04-14 09:58:53 +0700

    create the Pay with PayPal button component

---
+++ src/components/PayPalButton.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+interface PayPalButtonProps {
+  amount: string;
+  currency: string;
+  description: string;
+}
+const PayPalButton: React.FC<PayPalButtonProps> = ({
+  amount,
+  currency,
+  description,
+}) => {
+  //   console.log("amount in paypalbutton", amount);
+  const navigate = useNavigate();
+  const handleClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    try {
+      e.preventDefault();
+      const response = await fetch(
+        `http://localhost:8081/payment/create?method=paypal&amount=${amount}&currency=${currency}&description=${description}`,
+        {
+          method: "POST",
+        }
+      );
+      if (!response.ok) {
+        throw new Error("failed to fetch hello from Spring Boot");
+      }
+      const approvalUrl = await response.text();
+      console.log("approval url is", approvalUrl);
+      sessionStorage.setItem("approvalUrl", approvalUrl);
+      window.location.href = approvalUrl; // Redirect user to PayPal approval page
+      //   navigate(`window.location.origin/payment/success/${approvalUrl}`); // Redirect user to PayPal approval page
+      sessionStorage.clear();
+    } catch (error) {
+      console.error("error fetching hello from Spring Boot", error);
+    }
+  };
+  return (
+    <button className="btn btn-info" onClick={handleClick}>
+      Pay with PayPal
+    </button>
+  );
+};
+
+export default PayPalButton;
